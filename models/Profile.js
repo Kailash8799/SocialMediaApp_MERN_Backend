c44d01fa66f7d2faa37a621ae7cb7ec946d72af4
kailash8799 <kailashrajput8799@gmail.com>
2023-09-10 11:59:22 +0530

    some chnages

--- models/Profile.js
+++ models/Profile.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ProfileSchema = mongoose.Schema({
-    userid:{type:String,required:true,unique:true},
-    username:{type:String,required:true,unique:true},
-    useremail:{type:String,required:true,unique:true},
-    followers:{type:Array},
-    following:{type:Array},
-    savedpost:{type:Array},
-    videos:{type:Array},
-    profileImage:{type:String},
-    tweets:{type:Array},
-    images:{type:Array}
-},{timestamps: true})
+const ProfileSchema = mongoose.Schema(
+  {
+    userid: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    useremail: { type: String, required: true, unique: true },
+    followers: { type: Array },
+    following: { type: Array },
+    savedpost: { type: Array },
+    videos: { type: Array },
+    profileImage: { type: String },
+    tweets: { type: Array },
+    images: { type: Array },
+  },
+  { timestamps: true }
+);
 
-const Profile = mongoose.model("Profile",ProfileSchema)
+const Profile = mongoose.model("Profile", ProfileSchema);
 
-module.exports = Profile
-
+module.exports = Profile;
