acbc7eceb2311a00091517279510ecf0557640d0
kailash8799 <kailashrajput8799@gmail.com>
2023-09-26 01:09:02 +0530

    added header in app

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://linkagenetwork.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // app.use(express.json());  
 ConnectDb()
 app.use("/api/auth",require("./routes/auth"))
