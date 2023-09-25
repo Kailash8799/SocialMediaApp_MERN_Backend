9351f4985846c243b25bc280d0afcc6d03a810ae
kailash8799 <kailashrajput8799@gmail.com>
2023-09-26 00:31:20 +0530

    changes some api route

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());  
+// app.use(express.json());  
 ConnectDb()
 app.use("/api/auth",require("./routes/auth"))
 app.use("/api/addpost",require("./routes/addpost"))
